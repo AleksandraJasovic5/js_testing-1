a279dae674a59374fb890d36b8b429f57b0771b9
AleksandraJasovic5 <93889728+AleksandraJasovic5@users.noreply.github.com>
2021-12-21 09:01:25 +0100

    Update unit.test.js

--- test/unit.test.js
+++ test/unit.test.js
@@ -9,4 +9,10 @@ describe('Maths test', function() {
     it('Test if 1 - 1 = 0', function() {
         assert.equal(MyMath.sub(1, 1), 0);
     });
+       it('Test if 16 / 4 = 4', async function() {
+        assert.equal(MyMath.divide(16,4), 4);
+    });
+    it('Test if 2 * 3 * 5 = 30', function() {
+        assert.equal(MyMath.multiply(2, 3, 5), 30);
+    });
 });
